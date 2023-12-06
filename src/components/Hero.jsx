695338c2846800315294c0459203b1c8e8e3f6eb
Geetanj <geetanj16sharma@gmail.com>
2023-12-07 02:25:53 +0530

    Made changes in padding

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Geetanj</span></h1>
+          <h1 className={`${styles.heroHeadText} text-white`}> Hi, I'm <span className="text-[#915eff]"> Geetanj </span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a front-end developer <br className="sm:block hidden" /> I am a Certified Cloud Practitioner and I like to make full-stack projects
           </p>
@@ -26,14 +26,14 @@ const Hero = () => {
 
       <div className = "absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
       <a href="#about">
-        <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+        <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
           <motion.dev
             animate = {{
               y:[0, 24, 0]
             }}
             transition={{ duration : 1.5 , repeat:Infinity,
             repeatType: "loop"}}
-            className="w-2 h-3 rounded-full bg-secondary mb-1"
+            className="w-3 h-3 rounded-full bg-secondary mb-1"
           />
 
         </div>
